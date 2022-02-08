3c6c0759c49ac38e38982df0ccef589be6ac95fc
Sahadat Hossain <sahadat_hossain@appscode.com>
2022-02-08 11:04:14 +0600

    Make git cred pointer type
    Signed-off-by: Sahadat Hossain <sahadat_hossain@appscode.com>

--- api/v1alpha1/moduledefinition_types.go
+++ api/v1alpha1/moduledefinition_types.go
@@ -24,7 +24,7 @@ import (
 
 type Git struct {
 	Ref  string                `json:"ref"`
-	Cred apiv1.ObjectReference `json:"cred,omitempty"`
+	Cred *apiv1.ObjectReference `json:"cred,omitempty"`
 }
 
 type ModuleRef struct {
